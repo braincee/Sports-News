68bbe57e5fcada460083239b7ff40b2cc0809189
braincee <kwesi938@gmail.com>
2022-04-12 10:52:07 +0100

    Add linter error checkings, updated README and package.json

--- README.md
+++ README.md
@@ -1,1 +1,114 @@
-# Sports-News
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# AFRICAN WOMEN SOFTWARE ENGINEERS
+
+## Description
+
+OLYMPICS 2022 website is a major international multi-sport event held once every four years. The mordern Olympics were inspired by the ancient Olympics games held in Greece. I developed this website to show the wonderful games being payed during this period of Olympics.
+
+- This is the page view:
+
+    Desktop version: 
+    
+    Mobile Version:
+
+> Learning Objectives
+
+- Use semantic HTML tags.
+- Apply best practices in HTML code.
+- Use CSS box model.
+- Use CSS selectors correctly.
+- Use Flexbox to place elements in the page.
+- Demonstrate ability to create UIs adaptable to different screen - sizes using media queries.
+- Use GitHub Pages to deploy web pages.
+- Apply JavaScript best practices and language style guides in code.
+- Use JavaScript to manipulate DOM elements.
+- Use JavaScript events.
+- Use objects to store and access data.
+- Communicate technical concepts to other technical people.
+- Build the capstone project site.
+
+## Built With
+
+- HTML
+- CSS (Flexbox and Grid)
+- JavaScript
+- Git and GitHub
+
+## Live Demo
+
+[Live Demo Link]()
+
+## Zoom Video
+
+[Link Zoom]()
+
+## Getting Started
+
+To get a local copy for this project and running follow these simple example steps.
+
+### Prerequisites
+
+- You need to have git installed in your computer
+- A text editor or terminal
+- A web brower to view output
+
+### Setup
+
+- To clone the repository run this command `git clone https://github.com/braincee/Sports-News.git`
+
+### Run tests
+
+For tracking linters errors locally you need to follow these steps:
+
+- After cloning the project you need to run this command
+
+  > npm install
+  > `This command will download all the dependencies of the project`
+
+- For tracking the linter errors in HTML file run:
+
+  > npx hint .
+
+- For tracking the linter errors in CSS file run:
+
+  > npx stylelint "\*_/_.{css,scss}"
+
+  - And For tracking the linter errors in JavaScript file run:
+    > npx eslint .
+
+### Deployment
+
+- I used GitHub Pages to deploy my website
+- For more information about publishing sources, see "[About github page](https://docs.github.com/en/pages/getting-started-with-github-pages/about-github-pages#publishing-sources-for-github-pages-sites)"
+
+## Author
+
+👤 **Stephen Annor**
+
+- GitHub: [@braincee](https://github.com/braincee)
+- Twitter: [@annor0543](https://twitter.com/annor0543)
+- LinkedIn: [Stephen Annor](https://www.linkedin.com/in/kwesi-appiah-1387801a1/)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Design and inspiration by [Cindy Shin on Behance](https://www.behance.net/gallery/29845175/CC-Global-Summit-2015).
+
+- Hat tip to [Microvers](www.microverse.org) and all the staff
+- Thanks to My coding Partners
+- Thanks to My Morning-session-group and Standup-team Partners and
+- Thanks to Code Reviewers
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
--- functions/feature.js
+++ functions/feature.js
@@ -1,72 +1,70 @@
 const olympics = [
-    {
-      id: 1,
-      name: 'Olympics Games',
-      images: {
-        img: 'images/olympics-1.jpg',
-        alt: 'olympics',
-      },
-      sport: 'Opening Ceremony',
-      about: 'Gloria Tumushabe is the Founder and an instructor at Afro Fem Coders.She\'s a UC Berkeley computer science Master student from Uganda.',
+  {
+    id: 1,
+    name: 'Olympics Games',
+    images: {
+      img: 'images/olympics-1.jpg',
+      alt: 'olympics',
     },
-    {
-      id: 2,
-      name: 'Olympics News',
-      images: {
-        img: 'images/olympics-2.webp',
-        alt: 'olympics',
-      },
-      sport: 'Olympics Channel',
-      about: 'Lavanya is lead Instructor at Afro Fem Coders, a Master\'s student in Data Science ar UC Berkeley, a coding instructor at the coder school Berkeley, and author of programming courses on LinkedIn Learning.',
+    sport: 'Opening Ceremony',
+    about: 'Gloria Tumushabe is the Founder and an instructor at Afro Fem Coders.She\'s a UC Berkeley computer science Master student from Uganda.',
+  },
+  {
+    id: 2,
+    name: 'Olympics News',
+    images: {
+      img: 'images/olympics-2.webp',
+      alt: 'olympics',
     },
-    {
-      id: 3,
-      name: 'Swimming',
-      images: {
-        img: 'images/olympics-3.webp',
-        alt: 'olympics',
-      },
-      sport: 'Olympics Swimming',
-      about: 'Milak is considered the only swimmer with a chance at potentially spoiling things for Dressel, the heavy favorite.',
+    sport: 'Olympics Channel',
+    about: 'Lavanya is lead Instructor at Afro Fem Coders, a Master\'s student in Data Science ar UC Berkeley, a coding instructor at the coder school Berkeley, and author of programming courses on LinkedIn Learning.',
+  },
+  {
+    id: 3,
+    name: 'Swimming',
+    images: {
+      img: 'images/olympics-3.webp',
+      alt: 'olympics',
     },
-    {
-      id: 4,
-      name: 'Ice Skiing',
-      images: {
-        img: 'images/olympics-4.jpg',
-        alt: 'olympics',
-      },
-      sport: 'Olympics Skiing',
-      about: 'Ice is much more slippery than snow and the lack of friction makes it hard to turn, stay upright and move confidently.',
+    sport: 'Olympics Swimming',
+    about: 'Milak is considered the only swimmer with a chance at potentially spoiling things for Dressel, the heavy favorite.',
+  },
+  {
+    id: 4,
+    name: 'Ice Skiing',
+    images: {
+      img: 'images/olympics-4.jpg',
+      alt: 'olympics',
     },
-    {
-      id: 5,
-      name: 'Boxing',
-      images: {
-        img: 'images/olympics-5.jpg',
-        alt: 'olympics',
-      },
-      sport: 'Olympics Boxing',
-      about: 'At the final bell of boxing competition at the 2020 Tokyo Olympic Games, Uzbekistan’s Bakhodir Jalolov let out a primal scream, and given the brutal, hard-hitting encounter he had just had with U.S. powerhouse Richard Torrez Jr.',
+    sport: 'Olympics Skiing',
+    about: 'Ice is much more slippery than snow and the lack of friction makes it hard to turn, stay upright and move confidently.',
+  },
+  {
+    id: 5,
+    name: 'Boxing',
+    images: {
+      img: 'images/olympics-5.jpg',
+      alt: 'olympics',
     },
-    {
-      id: 6,
-      name: 'Basketball',
-      images: {
-        img: 'images/olympics-6.jpg',
-        alt: 'olympics',
-      },
-      sport: 'Olympics Basketball',
-      about: 'Curry, like James, dealt with injuries throughout his career. In 2021, he missed time due to a fractured tailbone, but still played in 63 of a possible 72 games. Still, the 33-year-old is taking the summer off to continue to heal his body.',
+    sport: 'Olympics Boxing',
+    about: 'At the final bell of boxing competition at the 2020 Tokyo Olympic Games, Uzbekistan’s Bakhodir Jalolov let out a primal scream, and given the brutal, hard-hitting encounter he had just had with U.S. powerhouse Richard Torrez Jr.',
+  },
+  {
+    id: 6,
+    name: 'Basketball',
+    images: {
+      img: 'images/olympics-6.jpg',
+      alt: 'olympics',
     },
-  ];
-  
-  const teamContainer = document.querySelector('#Games .games');
-  
-  function createCard(dataInfo) {
-    const li = document.createElement('li');
-    li.className = 'card header-2';
-    li.innerHTML = `<div class="card-img">
+    sport: 'Olympics Basketball',
+    about: 'Curry, like James, dealt with injuries throughout his career. In 2021, he missed time due to a fractured tailbone, but still played in 63 of a possible 72 games. Still, the 33-year-old is taking the summer off to continue to heal his body.',
+  },
+];
+const teamContainer = document.querySelector('#Games .games');
+function createCard(dataInfo) {
+  const li = document.createElement('li');
+  li.className = 'card header-2';
+  li.innerHTML = `<div class="card-img">
           <img src="${dataInfo.images.img}" alt="${dataInfo.images.alt}">
         </div>
         <div class="card-body">
@@ -74,45 +72,37 @@ const olympics = [
           <h4>${dataInfo.sport}</h4>
           <p>${dataInfo.about}</p>
         </div>`;
-    return li;
+  return li;
+}
+olympics.forEach((info) => {
+  const card = createCard(info);
+  teamContainer.appendChild(card);
+});
+const teamList = document.querySelectorAll('#Games .games li');
+const moreBtn = document.querySelector('#Games button');
+const btnSpan = document.querySelector('#Games button span');
+const btnIcon = document.querySelector('#Games button i');
+let state = false;
+function displayMore() {
+  if (!state) {
+    teamList.forEach((team) => {
+      team.style.display = 'flex';
+    });
+    btnSpan.textContent = 'LESS';
+    btnIcon.classList.remove('fa', 'fa-chevron-down');
+    btnIcon.classList.add('fa', 'fa-chevron-up');
+    state = true;
+  } else {
+    teamList.forEach((team) => {
+      team.style.display = 'none';
+    });
+    teamList[0].style.display = 'flex';
+    teamList[1].style.display = 'flex';
+    teamList[2].style.display = 'flex';
+    btnSpan.textContent = 'MORE';
+    btnIcon.classList.remove('fa', 'fa-chevron-up');
+    btnIcon.classList.add('fa', 'fa-chevron-down');
+    state = false;
   }
-  
-  olympics.forEach((info) => {
-    const card = createCard(info);
-    teamContainer.appendChild(card);
-  });
-  
-  const teamList = document.querySelectorAll('#Games .games li');
-  const moreBtn = document.querySelector('#Games button');
-  const btnSpan = document.querySelector('#Games button span');
-  const btnIcon = document.querySelector('#Games button i');
-  
-  let state = false;
-  
-  function displayMore() {
-    if (!state) {
-      teamList.forEach((team) => {
-        team.style.display = 'flex';
-      });
-      btnSpan.textContent = 'LESS';
-      btnIcon.classList.remove('fa', 'fa-chevron-down');
-      btnIcon.classList.add('fa', 'fa-chevron-up');
-  
-      state = true;
-    } else {
-      teamList.forEach((team) => {
-        team.style.display = 'none';
-      });
-      teamList[0].style.display = 'flex';
-      teamList[1].style.display = 'flex';
-      teamList[2].style.display = 'flex';
-  
-      btnSpan.textContent = 'MORE';
-      btnIcon.classList.remove('fa', 'fa-chevron-up');
-      btnIcon.classList.add('fa', 'fa-chevron-down');
-      state = false;
-    }
-  }
-  
-  moreBtn.addEventListener('click', displayMore);
-  
+}
+moreBtn.addEventListener('click', displayMore);
